6015b7b2040d61ce8128050a5afa20768276a17f
Victor <victor.allisson95@gmail.com>
2020-06-15 17:46:23 -0300

    commit 2 - alteracoes do app

--- jogo.js
+++ jogo.js
@@ -1,5 +1,7 @@
 var altura = 0
 var largura = 0 
+var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -12,7 +14,31 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+    tempo -= 1
+    if(tempo<0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('vitoria')
+    }else{
+    document.getElementById('cronometro').innerHTML = tempo
+    }
+},1000)
+
 function posicaoRandomica(){
+
+    //remover mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+        
+        if(vidas > 3){
+            window.location.href="fim_de_jogo.html"
+        }else{
+        document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+        vidas ++
+        }
+    }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -30,6 +56,10 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
     
